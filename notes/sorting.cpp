29054f5a21fb324ffe6cfb74bd0df2df38aade67
R_Ruhanshi <vanshikagoyal1918@gmail.com>
2025-10-13 01:50:41 +0530

    Update sorting.cpp

--- notes/sorting.cpp
+++ notes/sorting.cpp
@@ -28,3 +28,36 @@ int main() {
   }
   return 0;
 }
+
+// Bubble sort
+void bubble_sort(int arr[], int n) {
+  for (int i = n - 1; i >= 0; i--) {
+    int didswap = 0;
+    for (int j = 0; j <= i - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+        didswap = 1;
+      }
+    }
+    if (didswap == 0) {
+      break;
+    }
+    cout << "Runs\n";
+  }
+}
+
+// Insertion sort
+void insertion_sort(int arr[], int n) {
+  for (int i = 0; i <= n; ++i) {
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      int temp = arr[j - 1];
+      arr[j - 1] = arr[j];
+      arr[j] = temp;
+      j--;
+      cout << "Runs\n"
+    }
+  }
+}
